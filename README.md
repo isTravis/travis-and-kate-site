dfbac75be0a069e99c937638b0dd35eaf9cd809f
Travis Rich <travis.s.rich@gmail.com>
2018-02-06 16:52:26 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Travis and Kate's Wedding Site
+
+To run locally, open a terminal window and navigate to the site folder.
+
+Run:
+```
+npm install
+npm start
+```
+
+Navigate to localhost:8000
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const server = require('gulp-server-livereload');
+
+gulp.task('sass', function () {
+	console.log('Running sass');
+	return gulp.src('./src/style.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('move', function () {
+	console.log('Running move');
+	return gulp.src('./src/**.*')
+		.pipe(gulp.dest('dist'));
+});
+gulp.task('watch', function() {
+	return gulp.watch('./src/**.*', ['sass', 'move'])
+});
+
+gulp.task('server', function() {
+	return gulp.src('./dist')
+		.pipe(server({ livereload: true }));
+});
+
+gulp.task('start', ['watch', 'server']);
