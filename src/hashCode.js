64fcef1987af17a9a1bb095606f0008d3a85bfd3
Travis Rich <travis.s.rich@gmail.com>
2018-04-30 12:44:19 -0400

    Add hashCode file

---
+++ src/hashCode.js
@@ -0,0 +1,26 @@
+const json = require('./rawInvites.json');
+
+var hash = function(s) {
+	var a = 1, c = 0, h, o;
+	if (s) {
+		a = 0;
+		for (h = s.length - 1; h >= 0; h--) {
+			o = s.charCodeAt(h);
+			a = (a<<6&268435455) + o + (o<<14);
+			c = a & 266338304;
+			a = c!==0?a^c>>21:a;
+		}
+	}
+	return String(a);
+};
+
+const withHashCode = json.map((item)=> {
+	return {
+		...item,
+		hash: hash(item.code),
+		code: undefined,
+	};
+});
+const uniques = [...new Set(withHashCode.map(item => item.hash))];
+console.log("Is Unique: ", uniques.length === withHashCode.length );
+console.log(JSON.stringify(withHashCode, null, 4));
